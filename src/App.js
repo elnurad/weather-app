bf9ce3c5266b9ac39ec944a88301a95f8722dd27
Elnura D <elnurab@gmail.com>
2022-09-18 17:13:09 +0800

    fetch and jsx

--- src/App.js
+++ src/App.js
@@ -3,22 +3,47 @@ import './App.css';
 
 function App() {
   
-  const [weather, setWeather] = useState(undefined);
+  const [weather, setWeather] = useState({});
+  const [tempUnit, setTempUnit] = useState('F');
+  const [location, setLocation] = useState('')
   const apiKey = '76213925c4e0771dbabb037e3a681e74'
+  useEffect(() => {
+    setLocation('Berlin');
+   
+    }, []);
 
-  const getWeather =(e)=> {
+  async function getWeather(e){
+    
+    let geoData = {};
     if(e.key==='Enter')
     {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=london&appid=${apiKey}`,{mode: 'cors'})
+    await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${e.target.value}&&appid=${apiKey}`,{mode: 'cors'})
       .then((response) => response.json())
-      .then((data) => console.log(`lat: ${data[0].lat},lon: ${data[0].lon}`));
+      .then((data) => {
+        console.log(`lat: ${data[0].lat},lon: ${data[0].lon}`)
+        geoData = data;
+        setLocation(geoData[0].name)
+        console.log(geoData)
+      });
     }
-    //fetch weather date and set weather to data
- 
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=imperial&appid=${apiKey}`,{mode: 'cors'})
+        .then((response) => response.json()) 
+        .then((data)=>{
+          setWeather(data)})
+          console.log(weather)
   }
+
   return (
     <div className="App">
       <input placeholder="enter location" onKeyPress={getWeather} />
+      <h1>{location}</h1>
+      <h1>{Object.keys(weather).length > 0 ? `${weather.main.temp} F°`  : ''}</h1>
+      <ul>
+        <li key={Math.random()}><p>Feels like: {weather.main.feels_like}</p></li>
+        <li key={Math.random()}><p>Humidity: {weather.main.humidity}</p></li>
+        <li key={Math.random}><p>Wind: {weather.wind.speed} mph</p></li>
+        <li key={Math.random}><p>Forecast: {weather.weather[0].description}</p></li>
+      </ul>
     </div>
   );
 }
